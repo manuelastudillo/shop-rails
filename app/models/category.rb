6e4566bee44328bc974a073dff573a85f6ae0849
borisatma91 <boris.atma@gmail.com>
2017-05-16 10:52:40 -0400

    validacion modificando atributo name

--- app/models/category.rb
+++ app/models/category.rb
@@ -12,7 +12,7 @@ class Category < ApplicationRecord
 	include ActiveModel::Validations
 
 	validates :name, presence: true
-	validates :nombre, length: {minimum: 2, maximum: 50, :message => " debe tener entre 2 y 50 caracteres"}
+	validates :name, length: {minimum: 2, maximum: 50, :message => " debe tener entre 2 y 50 caracteres"}
 
 	validates :abreviacion, length: {minimum: 2, maximum: 50, :message => " debe tener entre 2 y 50 caracteres"}
 
--- app/models/proveedor.rb
+++ app/models/proveedor.rb
@@ -1,7 +1,7 @@
 class Proveedor < ApplicationRecord
   belongs_to :comuna
 
-validates :nombre,  length: {minimum: 2, maximum: 50, :message => " debe tener entre 2 y 50 caracteres"}
+validates :name,  length: {minimum: 2, maximum: 50, :message => " debe tener entre 2 y 50 caracteres"}
 validates :abreviacion,  length: {minimum: 2, maximum: 50, :message => " debe tener entre 2 y 50 caracteres"}
 validates :razon_social,  length: {minimum: 2, maximum: 50, :message => " debe tener entre 2 y 50 caracteres"}
 validates :rut, rut: true
